f43a7847fbd6aa9021e633791104a36611fa3d11
Blake Robbins <blake@blakerobbins.dev>
2024-12-08 21:57:51 -0800

    Switch to a more traditional ts-node build and run

--- util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPtNow = getPtNow;
-/**
- * @returns time adjusted to PT time zone
- */
-function getPtNow() {
-    return new Date(new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }));
-}
